32dd5cc6f006a26014f8412b7860e442e1946c54
Joe Karlsson <joe.carlson@callmejoe.net>
2016-03-14 20:23:55 -1000

    Added new tests - NOT DONE YET

--- tests/objects.spec.js
+++ tests/objects.spec.js
@@ -14,11 +14,28 @@ const Script = new Vm.Script(IndexFileRaw);
 Script.runInNewContext(sandbox);
 
 Test('js-objects', suite => {
-  Test('Declare Two Variables', t => {
-    t.ok(sandbox.a, 'a variable exists.');
-    t.equals(typeof (sandbox.a), 'number', 'a variable is assigned to a random number.');
-    t.ok(sandbox.b, 'b variable exists.');
-    t.equals(typeof (sandbox.b), 'number', 'b variable is assigned to a random number.');
+  Test('Plain object and adding properties', t => {
+    t.ok(sandbox.plainBox, 'plainBox variable exists.');
+    t.equals(typeof (sandbox.plainBox), 'object', 'plainBox variable is an object.');
+    t.ok(sandbox.plainBox, {}, 'plainBox variable is an empty object.');
+    t.equals(Object.keys(sandbox.plainBox).length, 3, 'plainBox variable is an object with 3 properties');
+    t.equals(typeof (sandbox.plainBox.color), 'string', 'plainBox.color is a string');
+    t.equals(typeof (sandbox.plainBox.size), 'number', 'plainBox.size is a number');
+    t.equals((sandbox.plainBox.size) <= 20 && (sandbox.plainBox.size) >= 0, true, 'plainBox.size is a number between 0 and 20');
+    t.equals(Array.isArray(sandbox.plainBox.contents), true, 'plainBox.contents is an array');
+    t.deepEqual(sandbox.plainBox.contents, [], 'plainBox.contents is an empty array');
+    t.end();
+  });
+
+  Test('An object with properties declared line by line', t => {
+    t.ok(sandbox.stockCar, 'stockCar variable exists.');
+    t.equals(typeof (sandbox.stockCar), 'object', 'stockCar variable is an object.');
+    t.equals(Object.keys(sandbox.stockCar).length, 5, 'stockCar variable is an object with 5 properties');
+    t.equals(typeof (sandbox.stockCar.model), 'string', 'stockCar.model is a string');
+    t.equals(typeof (sandbox.stockCar.year), 'number', 'sandbox.year is a number');
+    t.equals((sandbox.stockCar.year) <= 2015 && (sandbox.stockCar.year) >= 2000, true, 'stockCar.year is a number between 2000 and less than 2016');
+
+
     t.end();
   });
 
