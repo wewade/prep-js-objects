046702800df60ec7d9c8db439ad30cc807ed52f3
wewade <wewade1506@gmail.com>
2016-09-15 12:13:28 -1000

    hang in there

--- objects.js
+++ objects.js
@@ -6,15 +6,16 @@
             - Add a property named `size` and set it to a number value between `0` and `20`.
             - Add a property named `contents` and set it to be an empty array.
  */
-            var plainBox ={
-                color: "red",
-                size: 10,
-                contents: []
-                            
-            };
+            var plainBox ={};
+                plainBox.color = "red" ;
+                plainBox.size = 10;
+                plainBox.contents = [];
+
+                console.log(plainBox);
                 console.log(plainBox.color);
                 console.log(plainBox.size);
                 console.log(plainBox.contents);
+
 
 /*
     # An object with properties declared line by line
@@ -30,7 +31,20 @@
             - add a key named `driver` and set it to be `null`.
             - add a key named `passengers` and set it to be an empty array.
  */
+                var stockCar ={
 
+                    model: "Ford",
+                    year: 2013,
+                    automaticTransmission: true,
+                    driver: null,
+                    passengers: [],
+
+                };
+                 console.log(stockCar.model);
+                 console.log(stockCar.year);
+                 console.log(stockCar.automaticTransmission);
+                 console.log(stockCar.driver);
+                 console.log(stockCar.passengers);
 
 /*
     Add new property inside a function
@@ -45,8 +59,21 @@
 
         Finally, invoke your new function while passing in the `plainPerson` object and additional agrument values, then store the return value in a variable named `completePerson`. Use `console.log` three times to print the entire object, just the value at `name`, and just the value at `age`.
  */
+            var plainPerson ={};
 
+            function buildPerson(person , nameString , age){
+                person.name = nameString;
+                person.age = age;
+                return person;
 
+            }
+            buildPerson(plainPerson ,  "del" ,  57);
+
+             var completePerson = buildPerson(plainPerson , "del" , 57);
+
+            console.log(completePerson);
+            console.log(completePerson.name);
+            console.log(completePerson.age);
 /*
     # Display values of objects that are inside an array
         Declare a new variable named `arrayOfObjects` and set it to be [this array of objects](https://gist.github.com/sgnl/958adf99007329d2e4ff).
@@ -68,7 +95,78 @@
             purchase total:  15.62
             =====
             ...
- */
+ */              var arrayOfObjects = [
+             {
+
+             id: 0, 
+            date: "Monday Jan 25 2015 2:01 PM", 
+             total: "279.38" 
+             }, 
+             { 
+             id: 1, 
+            date: "Monday Jan 27 2015 11:31 AM", 
+            total: "79.80" 
+             }, 
+            { 
+            id: 2, 
+             date: "Monday Feb 1 2015 7:56 AM", 
+             total: "15.62" 
+           }, 
+          { 
+            id: 3, 
+             date: "Monday Feb 1 2015 9:43 AM", 
+         total: "19.83" 
+          }, 
+          { 
+             id: 4, 
+            date: "Monday Feb 1 2015 11:08 PM", 
+             total: "56.69" 
+           }, 
+           { 
+             id: 5, 
+            date: "Monday Feb 13 2015 10:22 AM", 
+            total: "137.92" 
+          }, 
+           { 
+         id: 6, 
+             date: "Monday Feb 14 2015 6:54 PM", 
+             total: "938.65" 
+          }, 
+           { 
+             id: 7, 
+            date: "Monday Feb 14 2015 7:17 PM", 
+             total: "43.77" 
+          }, 
+          { 
+            id: 8, 
+            date: "Monday Feb 14 2015 7:18 PM", 
+             total: "28.54" 
+          }, 
+          { 
+             id: 9, 
+             date: "Monday Feb 14 2015 7:18 PM", 
+             total: "194.33" 
+           } 
+             ];
+         
+        function printProcessedOrders(orders){
+
+        for (var i = 0; i < orders.length; i++) {
+
+            console.log("====");   //  "====" dividing marks              
+            console.log("id" + i); // "id" iteration through loop
+            console.log("purchase date:" + orders[i].date); // extract order date from array
+            console.log("purchase total:" + orders[i].total); // extract total from array
+
+            
+            }
+         }   
+            
+            printProcessedOrders(arrayOfObjects);
+
+
+
+
 
 
 /*
